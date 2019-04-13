8bff5891e4a5068c0fbc6833447f5379871793e3
Stavros Filosidis <stavrosfil@gmail.com>
2019-04-13 16:44:48 +0300

    Clean commit without the iterTools implementation

--- superpermutation.py
+++ superpermutation.py
@@ -3,6 +3,7 @@ from itertools import permutations
 
 N = 7
 
+
 class node():
 
     def __init__(self, value=-1):
@@ -26,6 +27,7 @@ def createPermutations(n):
     # print(ans)
     return ans
 
+
 def searchForNumInIterations(cutNum, l):
     for i in range(len(l)):
         item = l[i]
@@ -34,7 +36,8 @@ def searchForNumInIterations(cutNum, l):
             return (item, i)
     return ('-1', '-1')
 
-def calc(l = []):
+
+def calc(l=[]):
     finalString = ''
     lastNum = l[0]
     finalString = lastNum
@@ -59,59 +62,16 @@ def calc(l = []):
     print(finalString)
     print(len(finalString))
     print('leek')
-            
 
 
 def removeList(a, b):
     return list(set(a)-set(b))
-
-# def createIterativeTree(root, level):
-#     tmp = root
-#     createTree(root, level)
-#     while level < N:
-#         for i in range(len(tmp.children)):
-#             createTree(tmp.children[i], level + 1)
-
-def createTree(root, level):
-    if level < N:
-        nums = removeList(range(N), root.usedNums)
-        # print(nums)
-        while len(nums) > 0:
-            for i in sorted(nums):
-                tmp = node()
-                tmp.value = i + 0
-                tmp.parent = root
-                tmp.usedNums = root.usedNums + []
-                tmp.usedNums.append(i)
-                tmp.level = level
-                nums.remove(i)
-                root.children.append(tmp)
-            for i in range(len(root.children)):
-                createTree(root.children[i], level + 1)
-            
-            
-def calculate(root):
-    answer = ''
-    tmp = root
-    while tmp.level < N - 1:
-        answer += str(tmp.children[0].value)
-        tmp = tmp.children[0]
-    print(answer)
 
 
 def main():
     root1 = node()
     permutationList = createPermutations(N)
     calc(permutationList)
-    # createTree(root1, 0)
-    # print(len(root1.children))
-    # for i in range(len(root1.children)):
-    #     tempNode = root1.children[i]
-    #     for j in range(len(tempNode.children)):
-    #         for k in range(len(tempNode.children[j].children)):
-    #             tempNode2 = tempNode.children[j].children[k]
-    #             print(tempNode2.value)
-    # calculate(root1)
 
 
 if __name__ == "__main__":
